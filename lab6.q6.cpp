8e03ae52d44a825e86400a3cd1ffeac3bfe3e639
fidasalim <42141530+fidasalim@users.noreply.github.com>
2018-09-12 18:12:11 +0000

    Update lab6.q6.cpp

--- lab6.q6.cpp
+++ lab6.q6.cpp
@@ -1,31 +1,53 @@
 //including libabry
 #include<iostream>
 using namespace std;
-//Askuser two numbers
-void func1(int &num1,int &num2){
+//funcion to show the outputs the sum of all even numbers between firstNumber and secondNumber.
+//looping
+int sumEvenNumbers(int num1,int num2){
+	int sum=0;
+	for (int i=num1+1;i<num2;i++){
+		if(i%2==0){
+			sum+=i;}}
+	return sum;
+}
+//funcion to show the outputs the sum of all odd numbers between firstNumber and secondNumber.
+//looping
+int sumOddNumbers(int num1,int num2){
+	int sum=0;
+	for (int i=num1+1;i<num2;i++){
+		if(i%2==1){
+			sum+=i;}}
+	return sum;
+}
+int sumSquareEvenNumbers(int num1,int num2){
+	int sum=0;
+	for (int i=num1+1;i<num2;i++){
+		if(i%2==0){
+			sum+=i*i;}}
+	return sum;
+}
+int sumSquareOddNumbers(int num1,int num2){
+	int sum=0;
+	for (int i=num1+1;i<num2;i++){
+		if(i%2==1){
+			sum+=i*i;}}
+	return sum;
+}
+		
+//main function
+int main() {
+	int num1,num2;
+//ASK USER
 	cout<<"Let the two numbers be ";
 	cin>>num1;
 	cin>>num2;
-}
-//funcion to show the outputs the sum of all even numbers between firstNumber and secondNumber.
-int sumEvenNumbers(int num1,int num2,int sum){
-	int i;
-	/*if(num1%2==0){*/
-		for (i=num1;i<num2;){
-			i=+2;
-			sum=+i;}
-		cout<<"the sum of even numbers between "<<num1<<" and "<<num2<<" is " <<sum<<endl;}
-	/*else {
-			num1=num1+1;
-			for (i=num1;i<num2;i=+1){
-			sum+=i;}
-		cout<<"the sum of even numbers between "<<num1<<" and "<<num2<<" is" <<sum<<endl;}	
-}*///main function
-int main(){
-	int num1,num2,sum;
-//ASK USER
-	func1(num1,num2);
-//PROVIDING THE SUM OF EVEN NUMBERS
-	sumEvenNumbers(num1,num2,sum);
+//PROVIDING THE SUM OF even NUMBERS
+	cout<<"the sum of even numbers between "<<num1<<" and "<<num2<<" is " <<sumEvenNumbers(num1,num2)<<endl;
+//PROVIDING THE SUM OF odd NUMBERS
+	cout<<"the sum of odd numbers between "<<num1<<" and "<<num2<<" is " <<sumOddNumbers(num1,num2)<<endl;
+//PROVIDING THE SUM OF squares of even NUMBERS
+	cout<<"the sum of squares of even numbers between "<<num1<<" and "<<num2<<" is " <<sumSquareEvenNumbers(num1,num2)<<endl;
+//PROVIDING THE SUM OF squares of odd NUMBERS
+	cout<<"the sum of  squares of odd numbers between "<<num1<<" and "<<num2<<" is " <<sumSquareOddNumbers(num1,num2)<<endl;
 	return 0;
 }	
